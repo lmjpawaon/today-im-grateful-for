0eae27f69c24d32a46bb3f0be32c1d8776e2ad38
louispawaon <lpawaon@gmail.com>
2023-12-05 13:29:18 +0800

    feat: create, update, delete for journal entries ok

--- src/app/api/journal/[journalId]/route.ts
+++ src/app/api/journal/[journalId]/route.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import { prisma } from "../../../../../lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
@@ -24,5 +24,59 @@ export async function GET(req: NextRequest, res: NextResponse) {
   } catch (error) {
     console.error("Error fetching journal entries:", error);
     return NextResponse.json({ error: "Internal Server Error" });
+  }
+}
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const urlParts = req.url.split("/");
+    const journalId = urlParts[urlParts.length - 1];
+    const data = await req.json(); // Read the JSON data from the request body
+
+    // Validate data (optional)
+    // You can add validation rules here to ensure required fields are present, etc.
+    // const errors = validateEntry(data);
+    // if (errors) return NextResponse.json({ errors }, { status: 400 });
+
+    const updatedEntry = await prisma.journalEntry.update({
+      where: {
+        id: parseInt(journalId), // Update the entry with the specified ID
+      },
+      data: {
+        // Map request data to prisma model fields
+        ...data,
+        // Add additional fields or transformations if needed (e.g., updatedAt)
+        // updatedAt: new Date(),
+        modifiedAt: new Date()
+      },
+    });
+
+    return NextResponse.json(updatedEntry);
+  } catch (error) {
+    console.error("Error updating journal entry:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const urlParts = req.url.split("/");
+    const journalId = urlParts[urlParts.length - 1];
+
+    // Validate data (optional)
+    // You can add validation rules here to ensure required fields are present, etc.
+    // const errors = validateEntry(data);
+    // if (errors) return NextResponse.json({ errors }, { status: 400 });
+
+    const deletedEntry = await prisma.journalEntry.delete({
+      where: {
+        id: parseInt(journalId), // Update the entry with the specified ID
+      }
+    });
+
+    return NextResponse.json(deletedEntry);
+  } catch (error) {
+    console.error("Error deleting journal entry:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
--- src/app/api/journal/route.ts
+++ src/app/api/journal/route.ts
@@ -1,5 +1,5 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { prisma } from "../../../../lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET() {
     try {
@@ -9,4 +9,30 @@ export async function GET() {
       console.error('Error fetching journal entries:', error);
       return NextResponse.json({ error: "Internal Server Error" }, {status:500});
     }
+}
+
+export async function POST(req: NextResponse) {
+  try {
+    const data = await req.json();
+
+    // Validate data (optional)
+    // You can add validation rules here to ensure required fields are present, etc.
+    // const errors = validateEntry(data);
+    // if (errors) return NextResponse.json({ errors }, { status: 400 });
+
+    const entry = await prisma.journalEntry.create({
+      data: {
+        // Map request data to prisma model fields
+        ...data,
+        // Add additional fields or transformations if needed
+        createdAt: new Date(),
+        modifiedAt: new Date()
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error creating journal entry:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
