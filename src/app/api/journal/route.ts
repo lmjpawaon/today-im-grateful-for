a6142e0dba99693692a5576c1a1064e200735bfc
louispawaon <lpawaon@gmail.com>
2023-12-09 10:52:14 +0800

    refactor(route): fixed get entries per day

--- src/app/api/journal/route.ts
+++ src/app/api/journal/route.ts
@@ -1,10 +1,43 @@
 import { prisma } from "../../../../lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import type { JournalType } from "../../../../lib/types/JournalType";
+
+interface GroupedEntries {
+  [date: string]: {
+    entries: JournalType[];
+  };
+}
 
 export async function GET() {
     try {
-      const entries = await prisma.journalEntry.findMany({});
-      return NextResponse.json(entries)
+      const entriesByDate = await prisma.journalEntry.findMany({
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      
+      const groupedEntries: GroupedEntries = {}
+      
+      for (const entry of entriesByDate) {
+        const date = entry.createdAt.toISOString().slice(0, 10);
+        if (!groupedEntries[date]) {
+          groupedEntries[date] = {
+            entries: [],
+          };
+        }
+        groupedEntries[date].entries.push({
+          id: entry.id.toString(), // Convert id to string if needed
+          title: entry.title ?? undefined,
+          content: entry.content,
+          createdAt: entry.createdAt,
+          modifiedAt: entry.modifiedAt,
+        });
+      }
+      
+      const groupedEntriesArray = Object.values(groupedEntries);
+      
+      console.log(groupedEntriesArray);
+      return NextResponse.json(groupedEntries)
     } catch (error) {
       console.error('Error fetching journal entries:', error);
       return NextResponse.json({ error: "Internal Server Error" }, {status:500});
@@ -26,7 +59,7 @@ export async function POST(req: NextResponse) {
         ...data,
         // Add additional fields or transformations if needed
         createdAt: new Date(),
-        modifiedAt: new Date()
+        modifiedAt: new Date(),
       },
     });
 
