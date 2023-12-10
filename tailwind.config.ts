45c4aed000c28dc8c73b0c18cb4feefebf93d5b9
louispawaon <lpawaon@gmail.com>
2023-12-10 22:21:03 +0800

    chore: added project color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,13 @@ const config: Config = {
       },
     },
   },
+  colors: {
+    'text': '#423b38',
+    'background': '#dbd7cc',
+    'primary': '#edb678',
+    'secondary': '#9a4f3c',
+    'accent': '#725c4f',
+   },
   plugins: [],
 }
 export default config
