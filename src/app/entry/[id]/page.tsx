14f8241b183114842d1289ba8db5a38d344df2d6
louispawaon <lpawaon@gmail.com>
2023-12-10 16:31:24 +0800

    refactor(entry page): added edit and update

--- src/app/entry/[id]/page.tsx
+++ src/app/entry/[id]/page.tsx
@@ -1,39 +1,72 @@
-import type { JournalType } from "../../../../lib/types/JournalType"
+"use client"
 
-const EntryDetails: React.FC<{params: {id: string}}> = async ({params}) =>{
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import type { JournalType } from "../../../../lib/types/JournalType";
+import { useEffect, useState } from "react";
 
-  const id  = params.id
+const EntryDetails: React.FC<{ params: { id: string } }> = ({ params }) => {
+  const id = params.id;
+  const router = useRouter();
+  const [entries, setEntries] = useState<JournalType[]>([]);
 
-  const fetchEntries = async () => {
-    const response = await fetch(`http://localhost:3000/api/journal/${id}`, {
-      next: {
-        revalidate: 0
-      }
-    });
-    return response.json()
+  useEffect(() => {
+    const fetchEntries = async () => {
+      const response = await fetch(`http://localhost:3000/api/journal/${id}`, {
+        next: {
+          revalidate: 0,
+        },
+      });
+      const data = await response.json();
+      setEntries(data);
+    };
+
+    fetchEntries();
+  }, [id]);
+
+  const handleDeleteEntry = async (id: string) => {
+    try {
+      await fetch(`http://localhost:3000/api/journal/${id}`, {
+        method: "DELETE",
+      });
+      console.log("Entry deleted successfully!");
+      setEntries(entries.filter(entry => entry.id !== id));
+      router.push("/entry");
+    } catch (error) {
+      console.error("Error deleting entry:", error);
+    }
   };
-
-  const entries = await fetchEntries();
 
   return (
     <div>
-      <h1>Journal Entry {id}</h1>  
+      <h1>Journal Entry {id}</h1>
       <ul>
         {entries.map((entry: JournalType) => (
-          <li key={entry.id}>{entry.content}{entry.createdAt && (
+          <li key={entry.id}>
+            {entry.content}
+            {entry.createdAt && (
+              <div>
+                Created At: {new Date(entry.createdAt).toISOString()}
+              </div>
+            )}
+            {entry.modifiedAt && (
+              <div>
+                Modified At: {new Date(entry.modifiedAt).toISOString()}
+              </div>
+            )}
             <div>
-              Created At: {new Date(entry.createdAt).toISOString()}
+              <Link href={`/entry/${entry.id}/edit`}>
+                Edit
+              </Link>
+              <button onClick={() => handleDeleteEntry(entry.id)}>
+                Delete
+              </button>
             </div>
-          )}
-          {entry.modifiedAt && (
-            <div>
-              Modified At: {new Date(entry.modifiedAt).toISOString()}
-            </div>
-          )}</li>
+          </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default EntryDetails
+export default EntryDetails;
