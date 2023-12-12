5821059db8e490847e10c5277aef21ac6566ac88
louispawaon <lpawaon@gmail.com>
2023-12-12 08:19:01 +0800

    refactor(backbtn): removed <

--- src/app/components/Reusable/BackButton.tsx
+++ src/app/components/Reusable/BackButton.tsx
@@ -6,8 +6,7 @@ const BackButton = () => {
     <Link href="/">
     <div className="absolute top-4 left-4 cursor-pointer">
         <div className="bg-primary text-white w-14 h-10 rounded-full flex items-center justify-center">
-          <span className="text-xl">{"<"}</span>
-          <span className="ml-2">Back</span>
+          <span>Back</span>
         </div>
       </div>
   </Link>
