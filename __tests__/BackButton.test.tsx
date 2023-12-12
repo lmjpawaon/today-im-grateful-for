6334d5a6921f3987e33c59cef2f89a764577d623
louispawaon <lpawaon@gmail.com>
2023-12-12 08:17:57 +0800

    refactor(test): change test

--- __tests__/BackButton.test.tsx
+++ __tests__/BackButton.test.tsx
@@ -5,12 +5,12 @@ import BackButton from "../src/app/components/Reusable/BackButton";
 describe("BackButton Component", () => {
     test("renders with BackButton", () => {
     render(<BackButton/>);
-    expect(screen.getByRole("link", { name: "< Back" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Back" })).toBeInTheDocument();
   });
 
   test("BackButton navigates to the correct URL", () => {
     render(<BackButton />);
-    const linkElement = screen.getByRole("link", { name: "< Back" });
+    const linkElement = screen.getByRole("link", { name: "Back" });
 
     fireEvent.click(linkElement);
     expect(window.location.pathname).toBe('/');
